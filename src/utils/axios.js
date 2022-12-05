2c11af9f114f8dc3ec00c29b207f5d4a647c8f0d
nguyenduyquang <nguyenduyquang@cityascom.vn>
2022-12-05 13:39:12 +0700

    new update

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 const instance = axios.create({
-  baseURL: 'https://q-social.onrender.com/',
+  baseURL: 'https://q-social-backend.vercel.app/',
 })
 export default instance
